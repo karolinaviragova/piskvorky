7e31628c1da70e78f20f190c793041da732e50a6
Karolina Viragova <karolinavi94@gmail.com>
2025-04-20 18:09:49 +0200

    switch of player

--- hra.js
+++ hra.js
@@ -3,15 +3,16 @@ const circleIcon = document.querySelector(".circle__icon");
 
 const tvojeFunkce = (event) => {
   event.target.disabled = true
+  
   if (currentPlayer === "circle") {
-    event.target.classList.add("board__field--circle")
-    circleIcon.innerHTML = `<img src="icons/circle.svg" alt="white circle" class="circle">`
-    currentPlayer = "cross"
-  } else if (currentPlayer === "cross") {
-    event.target.classList.add("board__field--cross")
     circleIcon.innerHTML = `<img src="icons/cross.svg" alt="white cross" class="cross">`
-    currentPlayer = "circle"
-  }
+     event.target.classList.add("board__field--circle")
+     currentPlayer = "cross"
+   } else if (currentPlayer === "cross") {
+     event.target.classList.add("board__field--cross")
+      circleIcon.innerHTML = `<img src="icons/circle.svg" alt="white circle" class="circle">`
+     currentPlayer = "circle"
+   }
 }
 
 const allButons = document.querySelectorAll(".game__field");
